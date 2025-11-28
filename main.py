6ed1472f6ce030affc5d42416000453c6d97e42c
hughgoodbody <52483196+hughgoodbody@users.noreply.github.com>
2025-11-28 16:55:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from nicegui import ui
+print(">>> PASSENGER IMPORTED NICEGUI FROM:", nicegui.__file__)
+print(">>> NICEGUI VERSION:", getattr(nicegui, '__version__', '(no version)'))
 from nicegui.app import app as nicegui_app
 from nicegui.ui_run_with import run_with
 
