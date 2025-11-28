a7d31cce0802081eeb1f96f7ae29ef175368bba6
hughgoodbody <52483196+hughgoodbody@users.noreply.github.com>
2025-11-28 16:07:38 +0000

    Replace ASGI app compatibility with WSGI middleware

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,8 +5,10 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
-# Import ASGI app from NiceGUI 2.x
+# Import ASGI app from NiceGUI
 from nicegui import app as asgi_app
 
-from asgiref.compatibility import guarantee_single_callable
-application = guarantee_single_callable(asgi_app)
+# Real ASGI → WSGI adapter from Starlette
+from starlette.middleware.wsgi import WSGIMiddleware
+
+application = WSGIMiddleware(asgi_app)
