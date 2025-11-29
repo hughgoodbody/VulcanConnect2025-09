2f60fa378de9c98143b6b08168245f2a061386ea
hughgoodbody <52483196+hughgoodbody@users.noreply.github.com>
2025-11-29 07:05:28 +0000

    Delete frontend directory

--- frontend/components/encode_button.py
+++
@@ -1,5 +0,0 @@
-from nicegui import ui
-from frontend.configuration_encoding import encode_config
-
-def encode_button(ui_state):
-    ui.button('Encode Configurations', on_click=lambda: encode_config(ui_state))
