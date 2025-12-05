6b8031346250f96c145facd49ca394018d4607ed
hughgoodbody <52483196+hughgoodbody@users.noreply.github.com>
2025-12-05 15:49:27 +0000

    Initialize Flask app with blueprints

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+# app/__init__.py
+
+from flask import Flask
+from .public_api import config_routes, export_routes, bom_routes
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+
+    # Register blueprints
+    app.register_blueprint(config_routes.config_bp)
+    app.register_blueprint(export_routes.export_bp)
+    app.register_blueprint(bom_routes.bom_bp)
+
+    return app
