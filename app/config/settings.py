4ccbe52f71a2bf1bb8991b75c109b18df01e4966
hughgoodbody <52483196+hughgoodbody@users.noreply.github.com>
2025-12-05 11:22:10 +0000

    Add settings configuration for Onshape API

---
+++ app/config/settings.py
@@ -0,0 +1,12 @@
+import os
+# app/config/settings.py
+
+DEVELOPMENT_MODE = True  # ❗ Set to False to use real Onshape API
+LOCAL_DATA_PATH = "local_data"  # folder where cached responses live
+CREDS_PATH = "creds.json"
+API_VERSION = 'v12'
+API_BASE = 'https://cad.onshape.com'
+
+#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+#CREDS_PATH = os.path.join(BASE_DIR, 'user_data', 'hugh', 'creds.json')
+#SUPPLIER_PATH = os.path.join(BASE_DIR, 'user_data', 'hugh', 'SupplierDetails.json')
