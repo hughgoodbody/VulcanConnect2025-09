2d1efd19d4438dde7ef8571585e3b8e5a754df66
hughgoodbody <52483196+hughgoodbody@users.noreply.github.com>
2025-12-07 14:19:26 +0000

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,9 +4,10 @@
 import sys
 import os
 
-# Ensure project root is on PYTHONPATH
-current_dir = os.path.dirname(os.path.abspath(__file__))
-if current_dir not in sys.path:
-    sys.path.insert(0, current_dir)
+# Add project root to sys.path
+project_root = os.path.dirname(os.path.abspath(__file__))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
 
-from app.main import app as application  # Flask WSGI app
+# Import Flask WSGI application
+from app.main import app as application
