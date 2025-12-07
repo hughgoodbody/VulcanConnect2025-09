163d114df453702a04ecca4f357412182c02ee86
hughgoodbody <52483196+hughgoodbody@users.noreply.github.com>
2025-12-07 14:41:38 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,11 @@ from .public_api import config_routes, export_routes, bom_routes
 def create_app() -> Flask:
     app = Flask(__name__)
 
+    # Root route required for Passenger health check
+    @app.route("/")
+    def home():
+        return "OK"
+
     # Register blueprints
     app.register_blueprint(config_routes.config_bp)
     app.register_blueprint(export_routes.export_bp)
