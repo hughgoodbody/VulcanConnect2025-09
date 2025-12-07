97e0b92ca882de636e266989428b2aaa9786684f
hughgoodbody <52483196+hughgoodbody@users.noreply.github.com>
2025-12-07 16:14:54 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from .public_api import config_routes, export_routes, bom_routes
 def create_app() -> Flask:
     app = Flask(__name__)
     #Debug remove later
-    @app.route("/VulcanConnect2025-09/debug/routes")
+    @app.route("/debug/routes")
     def debug_routes():
         return "<pre>" + "\n".join(sorted(str(r) for r in app.url_map.iter_rules())) + "</pre>"
 
@@ -15,20 +15,20 @@ def create_app() -> Flask:
     def home():
         return "OK"
 
-    # MOUNT API BLUEPRINTS UNDER /vulcan-connect/api/*
+    # MOUNT API BLUEPRINTS UNDER /api/*
     app.register_blueprint(
         config_routes.config_bp,
-        url_prefix="/VulcanConnect2025-09/api/config"
+        url_prefix="/api/config"
     )
 
     app.register_blueprint(
         export_routes.export_bp,
-        url_prefix="/VulcanConnect2025-09/api/export"
+        url_prefix="/api/export"
     )
 
     app.register_blueprint(
         bom_routes.bom_bp,
-        url_prefix="/VulcanConnect2025-09/api/bom"
+        url_prefix="/api/bom"
     )
 
     return app
