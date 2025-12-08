40f091eabccaae0e09100e3f84ea95ad11cec825
hughgoodbody <52483196+hughgoodbody@users.noreply.github.com>
2025-12-08 18:39:25 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ from . import create_app
 
 app = create_app()
 
+@app.route("/testapi")
+def testapi():
+    return {"status": "ok"}
+
 # Optional: for local dev: `python -m app.main`
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
