20723202ed604fe6419b772b9343baa26a2e56f0
hughgoodbody <52483196+hughgoodbody@users.noreply.github.com>
2025-12-09 15:47:43 +0000

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,11 +1,15 @@
 import os
 # app/config/settings.py
 
-DEVELOPMENT_MODE = True  # ❗ Set to False to use real Onshape API
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "").lower() == "true"
 LOCAL_DATA_PATH = "local_json_responses"  # folder where cached responses live
 CREDS_PATH = "user_data/hugh/creds.json"
-API_VERSION = 'v12'
-API_BASE = 'https://cad.onshape.com'
+API_VERSION = os.getenv("API_VERSION", "v12")
+API_BASE = os.getenv("API_BASE", "https://cad.onshape.com")
+ONSHAPE_ACCESS_KEY = os.getenv("ONSHAPE_ACCESS_KEY")
+ONSHAPE_SECRET_KEY = os.getenv("ONSHAPE_SECRET_KEY")
+
+
 
 #BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 #CREDS_PATH = os.path.join(BASE_DIR, 'user_data', 'hugh', 'creds.json')
