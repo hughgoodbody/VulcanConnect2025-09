46df8a68d7d779f5d5953b9c83b54cd2108bd84f
hughgoodbody <52483196+hughgoodbody@users.noreply.github.com>
2025-12-11 17:36:07 +0000

    Update materials_routes.py

--- app/public_api/materials_routes.py
+++ app/public_api/materials_routes.py
@@ -4,7 +4,7 @@ import os
 
 material_bp = Blueprint("materials", __name__)
 
-MATERIAL_FILE = "user_data/hugh/Common SFX Materials.csv"
+MATERIAL_FILE = "user_data/hugh/Common SFX Materials.json"
 
 @material_bp.get("/")
 def get_materials():
