4aad12e1d44b9341c9432438ca74a25130524f55
hughgoodbody <52483196+hughgoodbody@users.noreply.github.com>
2025-12-12 09:15:11 +0000

    Update job_routes.py

--- app/public_api/job_routes.py
+++ app/public_api/job_routes.py
@@ -109,13 +109,13 @@ def create_job():
 # ----------------------------------------------------------
 # POST /api/job/updateParts
 # ----------------------------------------------------------
-@job_bp.route("/updateParts", methods=["POST", "OPTIONS", "GET"])
+@job_bp.route("/updateParts", methods=["POST"])
 def update_parts():
     if request.method == "OPTIONS":
         return jsonify({"ok": True}), 200
 
     payload = request.json
-    print("Received updated parts:", payload)
+    #print("Received updated parts:", payload)
     return jsonify({"status": "ok", "updated": True})
 
 
